57910013e68e2900985455dfdd370f6672b6d7f8
= <=>
2025-03-06 15:04:06 -0300

    add screen tracer and smooth snake movement on screen

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Ana the Conda")
+screen.tracer(0)
 
 starting_position = [(0,0), (-20, 0), (-40,0)]
 
@@ -16,8 +18,12 @@ for position in starting_position:
     new_segment.goto(position)
     segments.append(new_segment)
 
+
+
 game_is_on = True
 while game_is_on:
+    screen.update()
+    time.sleep(0.1)
     for seg in segments:
         seg.forward(20)
 
